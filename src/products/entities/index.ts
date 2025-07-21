83dc4d541d1db8278b3bc29fe530dd4c0c486ec3
jhoaking <peredoroj@gmail.com>
2025-07-21 13:11:43 -0400

    creando relacion con otra tabla de imagenes usando typeORM

---
+++ src/products/entities/index.ts
@@ -0,0 +1,4 @@
+export { Product } from "./product.entity";
+export { ProductImage } from "./products-images.entity";
+
+
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -3,8 +3,10 @@ import {
   BeforeUpdate,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { ProductImage } from './products-images.entity';
 
 @Entity()
 export class Product {
@@ -45,8 +47,24 @@ export class Product {
   @Column('text')
   gender: string;
 
-  // tags
-  //images
+
+  @Column('text',{
+    array : true,
+    default : []
+  })
+  tags : string[];
+
+
+  @OneToMany(
+    () => ProductImage,
+    (productImage) =>productImage.product,
+    {cascade : true}
+  )
+  images?: ProductImage;
+
+
+
+
 
   @BeforeInsert()
   checkSlugInsert() {
