52db44ac5e99ddbd552d373b690ff6334294bf41
jhoaking <peredoroj@gmail.com>
2025-07-25 12:15:28 -0400

    carga de archivos ya guarddos en la debe hechos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
+import { join } from 'path';
+
+
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
@@ -16,9 +21,13 @@ import { FilesModule } from './files/files.module';
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
-      autoLoadEntities : true,
-      synchronize : true
+      autoLoadEntities: true,
+      synchronize: true,
     }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+
     ProductsModule,
     CommonModule,
     SeedModule,
